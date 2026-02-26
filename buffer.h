260f91d2ff73d0c8b0b631933a6329060c4f8b1d
Jim-Adak <zhouzhenfan5@gmail.com>
2026-02-27 01:31:19 +0800

    将buffer类中的参数实现

--- buffer.h
+++ buffer.h
@@ -2,7 +2,24 @@
 // Created by Adak Jim on 2026/2/27.
 //
 
-#ifndef TINYWEBSERVER_BUFFER_H
-#define TINYWEBSERVER_BUFFER_H
+#ifndef BUFFER_H
+#define BUFFER_H
+#include<iostream>
+#include<cstring> //prepare
+#include<unistd.h> //write
+#include<sys/uio.h>
+#include<vector> //readv
+#include<atomic>
+#include<assert.h>
 
-#endif //TINYWEBSERVER_BUFFER_H
+
+class Buffer {
+private:
+    char* BeginPtr_(); //buffer开头
+    const char* BeginPtr_() const;
+    void MakeSpace_(size_t len);
+
+    std::vector<char> buffer_;
+    std::atomic<size_t> readPos_; //读的下标
+    std::atomic<size_t> writePos_; //写的下标
+};
