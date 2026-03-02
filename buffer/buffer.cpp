20bb6c5abfc13a159abced673369aa8db8bf40c0
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-02 17:20:23 +0800

    //将fd的内容读到缓冲区，即writable的位置

--- buffer/buffer.cpp
+++ buffer/buffer.cpp
@@ -96,6 +96,29 @@ void Append(const Buffer& buff) {
     Append(buff.Peek(),buff.ReadableBytes());
 }
 
+//将fd的内容读到缓冲区，即writable的位置
+ssize_t Buffer::ReadFd(int fd, int *Errno) {
+    char buff[65535]; //栈区
+    struct iovec iov[2];
+    size_t writeable = WritableBytes(); //先记录能写多少
+    //分散读，保证数据全部读完
+    iov[0].iov_base = BeginWrite();
+    iov[0].iov_len = writeable;
+    iov[1].iov_base = buff;
+    iov[1].iov_len = sizeof(buff);
+
+    ssize_t len = readv(fd,iov,2);
+    if (len<0) {
+        *Errno = errno;
+    }else if (static_cast<size_t>(len)<=writeable) { //若len小于writable，说明写区可以容纳len
+        writePos_ += len;
+    }else {
+        writePos_ = buffer_.size(); //写区写满了，下移到最后
+        Append(buff,static_cast<size_t>(len - writeable));//剩余的长度
+    }
+   return len;
+}
+
 
 
 
