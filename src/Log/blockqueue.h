fbff197d681937dcc2764d619025ef976517da96
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-04 15:52:28 +0800

    Blockqueue类中private实现

--- src/Log/blockqueue.h
+++ src/Log/blockqueue.h
@@ -4,5 +4,20 @@
 
 #ifndef TINYWEBSERVER_BLOCKQUEUE_H
 #define TINYWEBSERVER_BLOCKQUEUE_H
+#include<deque>
+#include<condition_variable>
+#include<mutex>
+#include<sys/time.h>
+
+template<typename T>
+class BlockQueue {
+private:
+    deque<T> deq_; //底层数据结构
+    mutex mtx_; //锁
+    bool isClose_; //关闭标志
+    size_t capacity; //容量
+    condition_variable condConsumer_; //消费者条件变量
+    condition_variable condProducer_; //生产者条件变量
+};
 
 #endif //TINYWEBSERVER_BLOCKQUEUE_H
