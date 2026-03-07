c27e566f83fd2a31d89857c09f43d6616943ae1a
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-07 22:59:43 +0800

    创建Log.h,引入头文件

---
+++ src/Log/Log.h
@@ -0,0 +1,21 @@
+//
+// Created by Adak Jim on 2026/3/7.
+//
+
+#ifndef TINYWEBSERVER_LOG_H
+#define TINYWEBSERVER_LOG_H
+
+#include<mutex>
+#include<string>
+#include<thread>
+#include<sys/time.h>
+#include<string.h>
+#include<stdarg.h>
+#include<assert.h>
+#include<sys/stat.h>
+#include"blockqueue.h"
+#include"../buffer/buffer.h"
+
+
+
+#endif //TINYWEBSERVER_LOG_H
