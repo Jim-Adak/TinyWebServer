1a9603e0394d6d18c08720747479f231c9c4dec5
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-09 19:07:50 +0800

    Log析构函数实现

--- src/Log/Log.cpp
+++ src/Log/Log.cpp
@@ -6,7 +6,7 @@
 
 //构造函数
 Log::Log() {
-    fp = nullptr;
+    fp_ = nullptr;
     deque_ = nullptr;
     writeThread_ = nullptr;
     lineCount_ = 0;
@@ -14,3 +14,15 @@ Log::Log() {
     isAsync_ = false;
 }
 
+Log::~Log() {
+    while (!deque_->empty()) {
+        deque_->flush(); //唤醒消费者，处理掉剩下的任务
+    }
+    deque_->Close(); //关闭队列
+    writeThread_->join(); //等待当前线程完成手中的任务
+    if (fp_) { //冲洗文件缓冲区，关闭文件描述符
+        std::lock_guard<std::mutex> locker(mtx_); //
+        flush(); //清空缓冲区中的数据
+        fclose(fp_); //关闭日志文件
+    }
+}
