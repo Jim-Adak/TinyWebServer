1c01a666f3741540eafee9ce3317c7c7e57cf3ed
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-09 18:01:05 +0800

    Log构造函数实现

--- src/Log/Log.cpp
+++ src/Log/Log.cpp
@@ -2,3 +2,15 @@
 // Created by Confuse on 2026/3/9.
 //
 
+#include "Log.h"
+
+//构造函数
+Log::Log() {
+    fp = nullptr;
+    deque_ = nullptr;
+    writeThread_ = nullptr;
+    lineCount_ = 0;
+    toDay_ = 0;
+    isAsync_ = false;
+}
+
