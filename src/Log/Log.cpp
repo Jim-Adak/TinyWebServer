8a1053790583ab2e263eba392abea2ee8d46aed4
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-10 22:25:40 +0800

    write函数的一部分实现

--- src/Log/Log.cpp
+++ src/Log/Log.cpp
@@ -99,5 +99,57 @@ void Log::init(int level, const char *path, const char *suffix, int maxQueueCapa
 
 }
 
+void Log::write(int level, const char *format, ...) {
+    struct timeval now = {0,0};
+    gettimeofday(&now,nullptr);
+    time_t tSec = now.tv_sec;
+    struct tm *sysTime = localtime(&tSec);
+    struct tm t = *sysTime;
+    va_list vaList;
+
+    //日志日期 日志行数 如果不是今天或者行数超了
+    if (toDay_ !=t.tm_mday || (lineCount_ && (lineCount_ % MAX_LINES == 0))) {
+        std::unique_lock<std::mutex> locker(mtx_);
+        locker.unlock();
+
+        char newFile[LOG_NAME_LEN];
+        char tail[36] = {0};
+        snprintf(tail, 36, "%04d_%02d_%02d", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
+
+        if (toDay_ != t.tm_mday) {
+            snprintf(newFile,LOG_NAME_LEN - 72,"%s/%s%s",path_,tail,suffix_);
+            toDay_ = t.tm_mday;
+            lineCount_ = 0;
+        }else {
+            snprintf(newFile,LOG_NAME_LEN - 72,"%s/%s-%d%s",path_,tail,(lineCount_ /MAX_LINES),suffix_);
+        }
+
+        locker.lock();
+        flush();
+        fclose(fp_);
+        fp_ = fopen(newFile,"a");
+        assert(fp_ != nullptr);
+    }
+
+    //在buffer内生成一条对应的日志信息
+    {
+        std::unique_lock<std::mutex> locker(mtx_);
+        lineCount_++;
+        int n = snprintf(buff_.BeginWrite(),128,"%d-%02d-%02d %02d:%02d:%02d.%06ld ",
+                  t.tm_year + 1900,t.tm_mon + 1, t.tm_mday,
+                  t.tm_hour,t.tm_min,t.tm_sec,now.tv_usec);
+
+        buff_.HasWritten(n);
+        AppendLogLevelTitle_(level);
+
+        va_start(vaList,format);
+        int m = vsnprintf(buff_.BeginWrite(),buff_.WritableBytes(),format,vaList);
+        va_end(vaList);
+
+        buff_.HasWritten(m);
+    }
+}
+
+
 
 
