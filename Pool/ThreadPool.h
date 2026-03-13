64e74e9b0eadeddf63ddd36d5438efd0257df0d9
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-13 22:11:05 +0800

    在ThreadPool中的private中创立Pool结构体，利用智能指针创立pool_

--- Pool/ThreadPool.h
+++ Pool/ThreadPool.h
@@ -5,4 +5,25 @@
 #ifndef TINYWEBSERVER_THREADPOOL_H
 #define TINYWEBSERVER_THREADPOOL_H
 
+#include<queue>
+#include<mutex>
+#include<condition_variable>
+#include<functional>
+#include<thread>
+#include<assert.h>
+
+class ThreadPool {
+public:
+private:
+    //用一个结构体封装起来，方便调用
+    struct Pool {
+        std::mutex mtx_;
+        std::condition_variable cond_;
+        bool isClosed;
+        std::queue<std::function<void()>>tasks; //任务队列，函数类型为void()
+    };
+    std::shared_ptr<Pool> pool_;
+};
+
+
 #endif //TINYWEBSERVER_THREADPOOL_H
