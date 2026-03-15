bfefab4982ff36b316e5e3aad14e6d2b12498f8b
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-15 21:51:44 +0800

    添加了mysql初始化出错的判断

--- src/Pool/sqlconnpool.cpp
+++ src/Pool/sqlconnpool.cpp
@@ -9,3 +9,18 @@ SqlConnPool* SqlConnPool::Instance() {
     static SqlConnPool pool;
     return &pool;
 }
+
+void SqlConnPool::Init(const char* host,int port,
+              const char* user,const char* pwd,
+              const char* dbName,int connSize = 10) {
+    assert(connSize > 0);
+    for (int i=0;i<connSize;i++) {
+        MYSQL* conn = nullptr;
+        conn = mysql_init(conn);
+        if (!conn) {
+            LOG_ERROR("MySql init error!");
+            assert(conn);
+        }
+        conn = mysql_real_connect(conn,host,user,pwd,dbName,port,nullptr,0);
+    }
+}
