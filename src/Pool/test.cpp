cebc62ce0c24713de8b6534ec34bab9ca94e4730
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-17 18:58:44 +0800

    添加头文件并确保Linux下也可以运行

--- src/Pool/test.cpp
+++ src/Pool/test.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Confuse on 2026/3/17.
 //
+
+#include "../Log/Log.h"
+#include"../Pool/ThreadPool.h"
+//Linux下将以下代码开放
+// #include <features.h>
+//
+// #if __GLIBC__ == 2 && __GLIBC_MINOR__ < 30
+// #include <sys/syscall.h>
+// #define gettid() syscall(SYS_gettid)
+// #endif
