dc75940b345fd40ed60366661d4085eacd761f63
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-17 19:54:52 +0800

    Test里的SetLevel

--- src/Pool/test.cpp
+++ src/Pool/test.cpp
@@ -11,3 +11,16 @@
 // #include <sys/syscall.h>
 // #define gettid() syscall(SYS_gettid)
 // #endif
+
+void TestLog() {
+    int cnt = 0,level = 0;
+    Log::Instance()->init(level,"./testlog1",".log",0);
+    for (level = 3;level>=0;level--) {
+        Log::Instance()->SetLevel(level);
+        for (int j = 0;j<10000;j++) {
+            for (int i = 0;i<4;i++) {
+                LOG_BASE(i,"&s 11111111111 &d ===============","Test",cnt++);
+            }
+        }
+    }
+}
