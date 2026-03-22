01ccc6e7eaf77da8fc1e4af256aa7d7c03c95518
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-22 20:49:23 +0800

    IsKeepAlive函数实现

--- src/http/httprequest.cpp
+++ src/http/httprequest.cpp
@@ -1,3 +1,22 @@
 //
 // Created by Confuse on 2026/3/22.
 //
+
+#include"httprequest.h"
+
+
+const std::unordered_set<std::string> HttpRequest::DEFAULT_HTML{
+    "/index","/register","/login",
+    "/welcome","/video","/picture",
+};
+
+const std::unordered_map<std::string,int> HttpRequest::DEFAULT_HTML_TAG{
+    {"/register.html",0},{"/login.html",1},
+};
+
+bool HttpRequest::IsKeepAlive() const {
+    if (header_.count("Connection") == 1 ) {
+        return header_.find("Connection")->second == "keep-alive" && version_ =="1.1";
+    }
+    return false;
+}
