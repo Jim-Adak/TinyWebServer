32445d524359e1870186f810d7dd91e9aab1563c
Jim-Adak <zhouzhenfan5@gmail.com>
2026-03-23 22:29:57 +0800

    关机提交

--- src/http/httprequest.cpp
+++ src/http/httprequest.cpp
@@ -20,3 +20,42 @@ bool HttpRequest::IsKeepAlive() const {
     }
     return false;
 }
+
+//解析处理
+bool HttpRequest::parse(Buffer &buff) {
+    const char* CRLF[] = "\r\n"; //行结束符标志（回车换行）
+    if (buff.ReadableBytes() <=0) { //没有可读的字符
+        return false;
+    }
+    //读取数据
+    while (buff.ReadableBytes() && state_ != FINISH) {
+        //从buff中的读指针开始到读指针结束，这块区域是未读取得数据并去除“\r\n"，返回有效数据的行末指针
+        const char* lineEnd = std::search(buff.Peek(),buff.BeginWriteConst(),CRLF,CRLF + 2);
+        //转换为string类型
+        std::string line(buff.Peek(),lineEnd);
+        switch (state_) {
+            /*
+          有限状态机，从请求行开始，每处理完后会自动转入到下一个状态
+      */
+            case REQUEST_LINE:
+                if (!ParseRequestLine_(line)) {
+                    return false;
+                }
+                ParsePath_();
+                break;
+            case HEADERS:
+                if (buff.ReadableBytes() <= 2) {
+                    state_ = FINISH;
+                }
+                break;
+            case BODY:
+                ParseBody_(line);
+                break;
+            default:
+                break;
+        }
+        if (lineEnd == buff.BeginWrite()){break;} //读完了
+        buff.RetrieveUntil(lineEnd + 2); //跳过回车换行
+    }
+}
+
