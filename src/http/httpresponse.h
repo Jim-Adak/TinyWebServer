6574918fcf88c3526c92ccb3edb4a7536d91fe15
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-15 20:10:04 +0800

    创建httpresponse并引入头文件

---
+++ src/http/httpresponse.h
@@ -0,0 +1,16 @@
+//
+// Created by Adak Jim on 2026/4/15.
+//
+
+#ifndef TINYWEBSERVER_HTTPRESPONSE_H
+#define TINYWEBSERVER_HTTPRESPONSE_H
+#include<unordered_map>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<sys/mman.h>
+
+#include"../buffer/buffer.h"
+#include"../Log/Log.h"
+
+#endif //TINYWEBSERVER_HTTPRESPONSE_H
