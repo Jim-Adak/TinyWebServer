c49c9e7749ce3a9f67515bfd97717aae656aa9ea
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-17 13:23:37 +0800

    SUFFIX_TYPE,CODE_STATUS,CODE_PATH三个unordered_map的支持类型

---
+++ src/http/httpresponse.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Adak Jim on 2026/4/17.
+//
+
+#include"httpresponse.h"
+
+using namespace std;
+
+const unordered_map<string,string> HttpResponse::SUFFIX_TYPE = {
+    { ".html",  "text/html" },
+       { ".xml",   "text/xml" },
+       { ".xhtml", "application/xhtml+xml" },
+       { ".txt",   "text/plain" },
+       { ".rtf",   "application/rtf" },
+       { ".pdf",   "application/pdf" },
+       { ".word",  "application/nsword" },
+       { ".png",   "image/png" },
+       { ".gif",   "image/gif" },
+       { ".jpg",   "image/jpeg" },
+       { ".jpeg",  "image/jpeg" },
+       { ".au",    "audio/basic" },
+       { ".mpeg",  "video/mpeg" },
+       { ".mpg",   "video/mpeg" },
+       { ".avi",   "video/x-msvideo" },
+       { ".gz",    "application/x-gzip" },
+       { ".tar",   "application/x-tar" },
+       { ".css",   "text/css "},
+       { ".js",    "text/javascript "},
+};
+
+const unordered_map<int,string> HttpResponse::CODE_STATUS = {
+    {200,"OK"},
+    {400,"Bad Request"},
+    {403,"Forbidden"},
+    {404,"Not FOund"},
+};
+
+const unordered_map<int,string> HttpResponse::CODE_PATH = {
+    {400,"/400.html"},
+    {403,"/403/html"},
+    {404,"/404.html"},
+};
