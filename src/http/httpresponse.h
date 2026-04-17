7c8139ea64343f87899a958e04090de7fa1c3171
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-17 13:40:36 +0800

    httpresponse.h所有函数

--- src/http/httpresponse.h
+++ src/http/httpresponse.h
@@ -18,6 +18,15 @@ class HttpResponse {
 public:
     HttpResponse();
     ~HttpResponse();
+
+    void Init(const std::string& srcDir,std::string& path,bool isKeepAlive = false,int code = -1);
+    void MakeResponse(Buffer& buff);
+    void UnmapFile();
+    char* File();
+    size_t FIleLen() const;
+    void ErrorContent(Buffer& buff,std::string message);
+    int Code() const {return code_;}
+
 private:
     void AddStateLine_(Buffer &buff);
     void AddHeader_(Buffer &buff);
