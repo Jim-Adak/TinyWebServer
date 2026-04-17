52496c970db432c48192d315c055d3714f4efaec
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-17 12:21:55 +0800

    httpresponse.h写完

--- src/http/httpresponse.h
+++ src/http/httpresponse.h
@@ -5,12 +5,40 @@
 #ifndef TINYWEBSERVER_HTTPRESPONSE_H
 #define TINYWEBSERVER_HTTPRESPONSE_H
 #include<unordered_map>
-#include<fcntl.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<sys/mman.h>
+#include<fcntl.h> // open
+#include<unistd.h> // close
+#include<sys/stat.h> // stat
+#include<sys/mman.h> // mmap, munmap
 
 #include"../buffer/buffer.h"
 #include"../Log/Log.h"
+
+
+class HttpResponse {
+public:
+    HttpResponse();
+    ~HttpResponse();
+private:
+    void AddStateLine_(Buffer &buff);
+    void AddHeader_(Buffer &buff);
+    void AddContent_(Buffer &buff);
+
+    void ErrorHtml_();
+    std::string GetFIleType_();
+
+    int code_;
+    bool isKeepAlive_;
+
+    std::string path_;
+    std::string srcDir_;
+
+    char* mmFIle_;
+    struct stat mmFIleStat_;
+
+    static const std::unordered_map<std::string,std::string> SUFFIX_TYPE; //后缀类型集
+    static const std::unordered_map<std::string,std::string>CODE_STATUS; //编码状态集
+    static const std::unordered_map<std::string,std::string>CODE_PATH; //编码路径集
+
+};
 
 #endif //TINYWEBSERVER_HTTPRESPONSE_H
