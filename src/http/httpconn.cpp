4787bd59e6321707e0bf38b41aa0d6f7128e987f
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-21 20:02:11 +0800

    read返回http长度

--- src/http/httpconn.cpp
+++ src/http/httpconn.cpp
@@ -52,6 +52,21 @@ const char *HttpConn::GetIP() const {
     return inet_ntoa(addr_.sin_addr);
 }
 
+int HttpConn::GetPort() const {
+    return addr_.sin_port;
+}
+
+ssize_t HttpConn::read(int *saveErrno) {
+    ssize_t len = -1;
+    do {
+        len = readBuff_.ReadFd(fd_,saveErrno);
+        if (len <= 0) {
+            break;
+        }
+    }while (isET);
+    return len;
+}
+
 
 
 
