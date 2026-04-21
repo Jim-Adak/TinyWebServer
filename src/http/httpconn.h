b3e5df678256bcc481a159f233ec5a7291b65c01
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-21 13:52:53 +0800

    HttpConn的public

--- src/http/httpconn.h
+++ src/http/httpconn.h
@@ -25,6 +25,25 @@ public:
     void init(int sockFd,const sockaddr_in& addr);
     ssize_t read(int* saveErrno);
     ssize_t write(int* saveErrno);
+    void Close();
+    int GetFd() const;
+    int GetPort() const;
+    const char* GetIP() const;
+    sockaddr_in GetAddr() const;
+    bool process();
+
+    //写的总长度
+    int ToWriteBytes() {
+        return iov_[0].iov_len + iov_[1].iov_len;
+    }
+
+    bool IsKeepAlive() const {
+        return request_.IsKeepAlive();
+    }
+
+    static bool isET;
+    static const char* srcDir;
+    static std::atomic<int> userCount;
 private:
     int fd_;
     struct sockaddr_in addr_;
