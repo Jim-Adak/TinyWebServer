203c52e711d8108fee00e16eda7e80d6f9bf18fb
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-22 13:24:44 +0800

    引入heaptimer定时器并写入头文件

---
+++ src/heaptimer/heaptimer.h
@@ -0,0 +1,17 @@
+//
+// Created by Adak Jim on 2026/4/22.
+//
+
+#ifndef TINYWEBSERVER_HEAPTIMER_H
+#define TINYWEBSERVER_HEAPTIMER_H
+#include<queue>
+#include<unordered_map>
+#include<time.h>
+#include<arpa/inet.h>
+#include<algorithm>
+#include<functional>
+#include<assert.h>
+#include<chrono>
+#include"../log/log.h"
+
+#endif //TINYWEBSERVER_HEAPTIMER_H
