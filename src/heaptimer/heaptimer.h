fd01a20fd43975761976497f8698e1084bb83239
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-22 13:31:33 +0800

    定义回调函数和时间相关类型别名

--- src/heaptimer/heaptimer.h
+++ src/heaptimer/heaptimer.h
@@ -14,4 +14,9 @@
 #include<chrono>
 #include"../log/log.h"
 
+typedef std::function<void()> TimeoutCallBack;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds MS;
+typedef Clock::time_point TimeStamp;
+
 #endif //TINYWEBSERVER_HEAPTIMER_H
