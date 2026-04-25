2fa1ca3aa3efd9fd1620b3dcf704e8a543a729ef
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-25 21:52:22 +0800

    在Heaptimer.cpp中添加SwapNode_,siftup_

---
+++ src/heaptimer/heaptimer.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Adak Jim on 2026/4/25.
+//
+
+#include"heaptimer.h"
+
+void HeapTimer::SwapNode_(size_t i, size_t j) {
+    assert(i >= 0 && i < heap_.size());
+    assert(j >= 0 && j < heap_.size());
+    std::swap(heap_[i],heap_[j]);
+    ref_[heap_[i].id] = i;  //结点内部id所在索引位置也要变化
+    ref_[heap_[j].id] = j;
+}
+
+void HeapTimer::siftup_(size_t i) {
+    assert(i >= 0 && i<heap_.size());
+    size_t parent = (i-1) / 2;
+    while(parent >= 0) {
+        if (heap_[parent] > heap_[i]) {
+            SwapNode_(i,parent);
+            i = parent;
+            parent = (i - 1) / 2;
+        }else {
+            break;
+        }
+    }
+}
