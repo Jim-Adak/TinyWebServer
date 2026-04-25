c179d4391d65ab8c448f64cba32cef4a560045ca
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-25 10:40:45 +0800

    HeapTimer封装完成

--- src/heaptimer/heaptimer.h
+++ src/heaptimer/heaptimer.h
@@ -31,4 +31,25 @@ struct TimerNode {
     }
 };
 
-#endif //TINYWEBSERVER_HEAPTIMER_H
+class HeapTimer {
+public:
+    HeapTimer(){ heap_.reserve(64); } //保留（扩充）容量
+    ~HeapTimer() { clear(); }
+    void adjust(int id,int newExpires);
+    void add(int id,int timeOut,const TimeoutCallBack& cb);
+    void doWork(int id);
+    void clear();
+    void tick();
+    void pop();
+    int GetNextTick();
+private:
+    void del_(size_t i);
+    void siftup_(size_t i);
+    bool siftdown_(size_t i,size_t n);
+    void SwapNode_(size_t i,size_t j);
+    std::vector<TimerNode> heap_;
+    //key : id value : vector的下标
+    std::unordered_map<int,size_t> ref_; //id对应的在heap_中的下标，方便用heap_的时候查找
+};
+
+#endif //TINYWEBSERVE_HEAPTIMER_H
