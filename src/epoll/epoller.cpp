501427304e6197375e162bfdfefb9ebda17c389b
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-26 19:23:39 +0800

    构造函数RAII

---
+++ src/epoll/epoller.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Adak Jim on 2026/4/26.
+//
+
+#include "epoller.h"
+
+Epoller::Epoller(int maxEvent) : epollFd_(epoll_create(512)),events_(maxEvent) {
+    assert(epollFd_ >= 0 && events_.size() > 0);
+}
