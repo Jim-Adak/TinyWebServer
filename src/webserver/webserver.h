c34cec6c175563ed9ad052c5f4d2be101edf1821
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-27 20:27:48 +0800

    创建WebServer类，定义所有变量

--- src/webserver/webserver.h
+++ src/webserver/webserver.h
@@ -21,6 +21,22 @@
 #include"../pool/ThreadPool.h"
 #include"../http/httpconn.h"
 
+class WebServer {
+private:
+    int port_;
+    bool openLinger_;
+    int timeoutMS_; //毫秒MS
+    bool isClose_;
+    int listenFd_;
+    char* srcDir_;
 
+    uint32_t listenEvent_; //监听事件
+    uint32_t connEvent_; //连接事件
+
+    std::unique_ptr<HeapTimer> timer_;
+    std::unique_ptr<ThreadPool> thread_pool_;
+    std::unique_ptr<Epoller> epoller_;
+    std::unordered_map<int,HttpConn> users_;
+};
 
 #endif //TINYWEBSERVER_WEBSERVER_H
