22f9ae2fa2fd3a16064634bd76f0f05ebd2742c5
Jim-Adak <zhouzhenfan5@gmail.com>
2026-04-27 21:26:45 +0800

    构造函数RAII

--- src/webserver/webserver.cpp
+++ src/webserver/webserver.cpp
@@ -1,3 +1,18 @@
 //
 // Created by Adak Jim on 2026/4/27.
 //
+
+#include "webserver.h"
+
+using namespace std;
+
+WebServer::WebServer(int port, int trigMode, int timeoutMS, bool PotLinger,
+    int sqlPort, const char *sqlUser, const char *sqlPwd, const char *dbName,
+    int connPoolNum, int threadNum, bool openLog, int logLevel, int logQueSize) :
+    port_(port),openLinger_(trigMode),timeoutMS_(timeoutMS),isClose_(false),
+    timer_(new HeapTimer()),thread_pool_(new ThreadPool(threadNum)),epoller_(new Epoller)
+{
+
+}
+
+
