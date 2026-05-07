2ea3bfaa84a6b6f7ffe53486c44f81ffca72ee73
Jim-Adak <zhouzhenfan5@gmail.com>
2026-05-07 21:40:15 +0800

    httpresponse.cpp里Init函数修改

--- src/http/httpresponse.cpp
+++ src/http/httpresponse.cpp
@@ -53,7 +53,7 @@ HttpResponse::~HttpResponse() {
     UnmapFile();
 }
 
-void HttpResponse::Init(const std::string& srcDir,string& path,bool isKeepAlive,int code) {
+void HttpResponse::Init(const std::string& srcDir,const std::string& path,bool isKeepAlive,int code) {
     assert(srcDir != "");
     if (mmFile_) { UnmapFile(); }
     code_ = code;
