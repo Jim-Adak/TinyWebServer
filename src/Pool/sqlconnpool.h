cf103b2d832bcd50cc58eab9a4af126d81a1add7
Jim-Adak <zhouzhenfan5@gmail.com>
2026-05-21 17:26:50 +0800

    SqlConnRAII析构实现

--- src/Pool/sqlconnpool.h
+++ src/Pool/sqlconnpool.h
@@ -43,6 +43,11 @@ public:
         sql_ = *sql;
         connPool_ = connpool;
     }
+    ~SqlConnRAII() {
+        if (sql_)
+            connPool_->FreeConn(sql_);
+    }
+
 private:
     MYSQL *sql_;
     SqlConnPool *connPool_;
