fcf8d0df5a23d3363b3c8a3ea7874ab400d290d6
Jim-Adak <zhouzhenfan5@gmail.com>
2026-05-24 17:04:55 +0800

    重构HttpConn的write函数

--- src/http/httpconn.cpp
+++ src/http/httpconn.cpp
@@ -77,18 +77,32 @@ ssize_t HttpConn::read(int *saveErrno) {
 ssize_t HttpConn::write(int *saveErrno) {
     ssize_t len = -1;
     do {
-        len = writev(fd_,iov_,iovCnt_);
+        len = writev(fd_,iov_,iovCnt_);  //将iov的内容写到fd中
         if (len <= 0) {
             *saveErrno = errno;
             break;
         }
-        if (iov_[0].iov_len + iov_[1].iov_len == 0){ break; } //传输结束
+        if (iov_[0].iov_len + iov_[1].iov_len == 0)
+            break;    //传输结束
         else if (static_cast<size_t>(len) > iov_[0].iov_len) {
-            iov_[1].iov_base =(char*)iov_[1].iov_base +(len - iov_[0].iov_len);
+            iov_[1].iov_base = (uint8_t*) iov_[1].iov_base + (len - iov_[0].iov_len);
+            iov_[1].iov_len -= (len - iov_[0].iov_len);
+            if (iov_[0].iov_len) {
+                writeBuff_.RetrieveAll();
+                iov_[0].iov_len = 0;
+            }
+        }
+        else {
+            iov_[0].iov_base = (uint8_t*)iov_[0].iov_base + len;
+            iov_[0].iov_len -= len;
+            writeBuff_.Retrieve(len);
         }
     }while (isET || ToWriteBytes() > 10240);
     return len;
 }
+
+
+
 
 bool HttpConn::process() {
     request_.Init();
